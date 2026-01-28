589c0ece9bdd44b55d490ba35e35fe76adb35296
EstherLupton <esther.lupton@students.plymouth.ac.uk>
2026-01-28 15:15:06 +0000

    Added conseptual code for LSB embedding

--- backend/services/embeddings.services.js/lsb.services.js
+++ backend/services/embeddings.services.js/lsb.services.js
@@ -1,5 +1,62 @@
-async function lsbEmbed(imagePath, hiddenData){
+import sharp from 'sharp';
 
-    return imageEmbeddedPath
+async function lsbEmbed(imagePath, hiddenData){
+    const image = sharp(imagePath);
+    const pixels = await image.raw().toBuffer({ resolveWithObject: true });
+    let messageBinary = messageToBinary(hiddenData);
+    messageBinary += "1111111111111110"; 
+
+    const channels = pixels.info.channels;
+    const maxCapacity = channels === 4? (pixels.data.length / 4) * 3 : pixels.data.length; 
+    if (messageBinary.length > maxCapacity) {
+        throw new Error('Message too long to embed in image');
+    }
+
+    let dataIndex = 0;
+    let newPixels = new Uint8Array(pixels.data.length);
+    for (let i = 0; i < pixels.data.length; i++) {
+        if ( channels === 4 && i % 4 === 3 ) {
+            newPixels[i] = pixels.data[i];
+            continue;
+        }
+        if (dataIndex < messageBinary.length) {
+            let pixelByte = pixels.data[i];
+            let bit = messageBinary[dataIndex];
+            pixelByte = (pixelByte & 0xFE) | parseInt(bit);
+            newPixels[i] = pixelByte;
+            dataIndex++;
+        } else {
+            newPixels[i] = pixels.data[i];
+        }
+    }
+
+    const imageEmbedded = createImageFromPixels(newPixels, pixels.info);
+    return imageEmbedded;
+}
+
+function messageToBinary(message) {
+    let binaryMessage = '';
+    for (let i = 0; i < message.length; i++) {
+        const asciiValue = message.charCodeAt(i);
+        binaryMessage += asciiValue.toString(2).padStart(8, '0');
+    }
+    return binaryMessage;
+}
+
+async function createImageFromPixels(pixelData, info) {
+    const { width, height, channels } = info;
+    const outputDirectory = 'backend/uploadsstegged';
+    const outputPath = outputDirectory + '/stegged_' + Date.now() + '.png';
+
+    const newImage = sharp(Buffer.from(pixelData), {
+        raw: {
+            width: width,
+            height: height,
+            channels: channels
+        }
+    });
+
+    await newImage.toFile(outputPath);
+    return outputPath;
 }
 
