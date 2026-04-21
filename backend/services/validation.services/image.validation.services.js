b0c24bc59034056e64c0f42575d0783470e93247
EstherLupton <esther.lupton@students.plymouth.ac.uk>
2026-04-21 12:03:13 +0100

    update max image size to 15 mb

--- backend/services/validation.services/image.validation.services.js
+++ backend/services/validation.services/image.validation.services.js
@@ -29,7 +29,7 @@ export async function validateImage(fileBuffer, fileName, mimeType) {
 
     const maxWidth = 10000;
     const maxHeight = 10000;
-    const maxFileSize = 12 * 1024 * 1024; // 12 Mb
+    const maxFileSize = 15 * 1024 * 1024; // 15 Mb
 
     if (metadata.width > maxWidth || metadata.height > maxHeight) {
         throw new Error('Image dimensions exceed allowed limits');
